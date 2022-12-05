db6aab6f994e61b535bdb996532675b9f38880c6
YOUR-USERNAME <gtellez07@yahoo.com>
2022-12-05 09:46:49 -0600

    wordlist

--- script.js
+++ script.js
@@ -1,4 +1,73 @@
-
+// console.log("are you working?")
+// list of 6 letter words via https://byjus.com/english/6-letter-words/
+const wordList = [
+    {
+        word: "august",
+        hint: "the name of a month"
+    },
+    {
+        word: "circle",
+        hint: "the shape of the sun"
+    },
+    {
+        word: "decade",
+        hint: "ten years long"
+    },
+    {
+        word: "afraid",
+        hint: "another word for scared"
+    },
+    {
+        word: "coffee",
+        hint: "alot of people drink this in the morning"
+    },
+    {
+        word: "annual",
+        hint: "another word for every year"
+    },
+    {
+        word:"device",
+        hint:"a title for a cellphone or computer"
+    },
+    {
+        word:"camera",
+        hint:"i use this to take pictures"
+    },
+    {
+        word:"doctor",
+        hint:"someone i go see when i am feeling sick"
+    },
+    {
+        word:"dollar",
+        hint:"in mexico the currency is pesos, in the US the currency is..."
+    },
+    {
+        word:"branch",
+        hint:"a part that is connected to a tree"
+    },
+    {
+        word:"bottle",
+        hint:"i drink soda out of a ..."
+    },
+    {
+        word:"bright",
+        hint:"another word for smart, also a word to describe the sun"
+    },
+    {
+        word:"bridge",
+        hint:"something you can cross over"
+    },
+    {
+        word:"double",
+        hint:"comes before triple"
+    }, {
+        word:"career",
+        hint:"another term for a job"
+    }, {
+        word:"driver",
+        hint:"someone who can maneuver a vehicle"
+    },
+]
 
 
 
